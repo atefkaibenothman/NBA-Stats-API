47523f4caccc0d0cd50c8123271b1bde161d316a
Kai <abenothm@uci.edu>
2019-12-10 17:07:05 -0800

    minor changes

--- launch.py
+++ launch.py
@@ -1,14 +1,31 @@
 from data.database import Database
-from utils.download import download_all_players
+from utils.download import download_all_players, download_player_game_log
+
+# GLOBALS
+SEASON = "2019-2020"
+SEASON_TYPE = "Regular Season"
+LEAGUE_ID = "00"
 
 def start():
+    """
+    starts the entire program
+    """
     # initialize a database
     db = Database()
+
     # download all player basic info 
     download_all_players(database=db)
-    # lists all the players
-    db.list_all_players()
+
+    return db
+
+def download_game_log(db):
+    """
+    iterates through all players and updates the database w/ updated statistics
+    """
+    for player_id in db:
+        download_player_game_log(player_id, SEASON, SEASON_TYPE)
+
 
 if __name__ == "__main__":
-    # starts the entire program
-    start()
+    db = start()
+    download_game_log(db)
--- utils/download.py
+++ utils/download.py
@@ -3,9 +3,12 @@ import requests
 from utils.response import Response
 
 from nba_api.stats.static import players
+from nba_api.stats import playergamelog
 
-# download all player basic info
 def download_all_players(database=None):
+    """
+    download all player basic info
+    """
     # call nba_api and get all the players
     all_players = players.get_players()
 
@@ -14,3 +17,12 @@ def download_all_players(database=None):
     
     # extract the response for only active players
     r.extract_active_players()
+
+
+def download_player_game_log(player_id):
+    """
+    download player game log
+    src: https://github.com/swar/nba_api/blob/master/docs/nba_api/stats/endpoints/playergamelog.md
+    """
+    a = playergamelog(player_id)
+    print(a)
